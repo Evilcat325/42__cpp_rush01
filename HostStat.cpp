bfa56c1ab97132b4f61a9a7b6ca1e17448d8856d
Sen Li <ls.learnshare@gmail.com>
2019-05-12 06:01:59 -0700

    using hoststa

--- HostStat.cpp
+++ HostStat.cpp
@@ -5,6 +5,32 @@ HostStat::HostStat()
 {
 	vm_count = HOST_VM_INFO64_COUNT;
 	load_count = HOST_CPU_LOAD_INFO_COUNT;
+	host_statistics64(curr_host, HOST_CPU_LOAD_INFO,
+										reinterpret_cast<host_info64_t>(&cpu_stat.p_cpu), &load_count);
+	loadCPUInfo();
+	loadHostInfo();
+}
+
+void HostStat::loadCPUInfo()
+{
+	/* reading brand string*/
+	char buf[128];
+	size_t len = 128;
+	size_t int_size = sizeof(int);
+	sysctlbyname("machdep.cpu.brand_string", &buf, &len, NULL, 0);
+	cpu_stat.brand_string = buf;
+	sysctlbyname("machdep.cpu.vendor", &buf, &len, NULL, 0);
+	cpu_stat.vendor = buf;
+	sysctlbyname("machdep.cpu.core_count", &cpu_stat.core_count, &int_size, NULL, 0);
+	sysctlbyname("machdep.cpu.thread_count", &cpu_stat.thread_count, &int_size, NULL, 0);
+}
+
+void HostStat::loadHostInfo()
+{
+	static char buf[1024];
+	gethostname(buf, 1024);
+	host_stat.hostname = buf;
+	host_stat.username = getenv("USER");
 }
 
 void HostStat::snapVM()
@@ -15,20 +41,63 @@ void HostStat::snapVM()
 
 void HostStat::snapCPUUsage()
 {
-	host_statistics(curr_host, HOST_CPU_LOAD_INFO,
-									reinterpret_cast<host_info64_t>(&host_stat.cpu), &load_count);
+	host_statistics64(curr_host, HOST_CPU_LOAD_INFO,
+										reinterpret_cast<host_info64_t>(&cpu_stat.cpu), &load_count);
+	unsigned long long userticks, systicks, idleticks;
+	double totalticks;
+	userticks = cpu_stat.cpu.cpu_ticks[CPU_STATE_USER] + cpu_stat.cpu.cpu_ticks[CPU_STATE_NICE];
+	systicks = cpu_stat.cpu.cpu_ticks[CPU_STATE_SYSTEM];
+	idleticks = cpu_stat.cpu.cpu_ticks[CPU_STATE_IDLE];
+	userticks -= (cpu_stat.p_cpu.cpu_ticks[CPU_STATE_USER] +
+								cpu_stat.p_cpu.cpu_ticks[CPU_STATE_NICE]);
+	systicks -= cpu_stat.p_cpu.cpu_ticks[CPU_STATE_SYSTEM];
+	idleticks -= cpu_stat.p_cpu.cpu_ticks[CPU_STATE_IDLE];
+	totalticks = userticks + systicks + idleticks + 0.0000001;
+	cpu_stat.usage_user = 100.0 * userticks / totalticks;
+	cpu_stat.usage_sys = 100.0 * systicks / totalticks;
+	cpu_stat.usage_idle = 100.0 * idleticks / totalticks;
+	cpu_stat.p_cpu = cpu_stat.cpu;
 }
 
 void HostStat::snapCPULoad()
 {
-	getloadavg(host_stat.loadavg, sizeof(host_stat.loadavg));
+	getloadavg(cpu_stat.loadavg, sizeof(cpu_stat.loadavg));
 }
 
 void HostStat::print()
 {
-	std::cout << std::fixed << std::setprecision(4);
-	std::cout << host_stat.loadavg[0] << " "
-						<< host_stat.loadavg[1] << " "
-						<< host_stat.loadavg[2] << " "
+	std::cout << std::fixed << std::setprecision(4)
+						<< cpu_stat.loadavg[0] << " "
+						<< cpu_stat.loadavg[1] << " "
+						<< cpu_stat.loadavg[2] << " "
+						<< std::endl
+						<< std::setprecision(2)
+						<< cpu_stat.usage_user << "% user\t"
+						<< cpu_stat.usage_sys << "% sys\t"
+						<< cpu_stat.usage_idle << "% idle\t"
+						<< std::endl
+						<< cpu_stat.brand_string << std::endl
+						<< cpu_stat.vendor << " vendor\t"
+						<< cpu_stat.core_count << " core\t"
+						<< cpu_stat.thread_count << " thread\t"
+						<< std::endl
+						<< host_stat.hostname << " "
+						<< host_stat.username
 						<< std::endl;
+}
+
+void HostStat::update()
+{
+	snapCPULoad();
+	snapCPUUsage();
+	snapVM();
+}
+
+std::string const &HostStat::get_hostname()
+{
+	return host_stat.hostname;
+}
+std::string const &HostStat::get_username()
+{
+	return host_stat.username;
 }
--- HostStat.hpp
+++ HostStat.hpp
@@ -7,17 +7,20 @@
 #include <sys/time.h>
 #include <iostream>
 #include <iomanip>
+#include <string>
+#include <sys/types.h>
+#include <sys/sysctl.h>
+#include <cstdlib>
+#include <unistd.h>
+#include <curl/curl.h>
 
 typedef struct
 {
+	std::string hostname;
+	std::string username;
+
 	/* Number of processes. */
 	uint32_t nprocs;
-
-	/* CPU loads. */
-	host_cpu_load_info_data_t cpu;
-
-	/* Load averages for 1, 5, and 15 minutes. */
-	double loadavg[3];
 
 	/* Total number of threads. */
 	uint32_t threads;
@@ -41,6 +44,26 @@ typedef struct
 	uint64_t disk_wbytes;
 } hoststat_t;
 
+typedef struct
+{
+	/* CPU loads. */
+	host_cpu_load_info_data_t cpu;
+	host_cpu_load_info_data_t p_cpu;
+
+	/* Load averages for 1, 5, and 15 minutes. */
+	double loadavg[3];
+	double usage_user;
+	double usage_sys;
+	double usage_idle;
+
+	/* sysctl only read once */
+	std::string brand_string;
+	std::string vendor;
+	int core_count;
+	int thread_count;
+
+} cpustat_t;
+
 class HostStat
 {
 private:
@@ -49,6 +72,7 @@ private:
 	unsigned int vm_count;
 	unsigned int load_count;
 	hoststat_t host_stat;
+	cpustat_t cpu_stat;
 	vm_statistics64_data_t vm_stat;
 
 	HostStat(HostStat const &rhs);
@@ -58,10 +82,20 @@ public:
 	HostStat();
 	~HostStat() {}
 
+	void loadCPUInfo();
+	void loadHostInfo();
 	void snapVM();
 	void snapCPUUsage();
 	void snapCPULoad();
+	void update();
 	void print();
+
+	std::string const &get_hostname();
+	std::string const &get_username();
+
+private:
+	static void cpu_percent(unsigned long long ticks, unsigned long long totalticks,
+													unsigned long long &whole, unsigned long long &part);
 };
 
 #endif /* FT_HostStat_HPP */
